f09ebfcb28f04810d73dee901e8b1a596d7a72c0
Iman Dissanayake <imandissanayake12@gmail.com>
2021-01-07 02:33:45 +0530

    table added for farmer profile

--- Web/src/app/farmer-profile/farmer-profile.component.ts
+++ Web/src/app/farmer-profile/farmer-profile.component.ts
@@ -9,6 +9,37 @@ export class FarmerProfileComponent implements OnInit {
 
   constructor() { }
 
+
+  headers = ['Product','Qty','Price'];
+
+  rows = [
+    {
+      "Product":"1",
+      "Qty":"50",
+      "Price":"100.00"
+    },
+    {
+      "Product":"2",
+      "Qty":"150",
+      "Price":"200.00"
+    },
+    {
+      "Product":"3",
+      "Qty":"500",
+      "Price":"1000.00"
+    },
+    {
+      "Product":"4",
+      "Qty":"50",
+      "Price":"500.00"
+    },
+    {
+      "Product":"5",
+      "Qty":"500",
+      "Price":"1500.00"
+    }
+  ]
+
   ngOnInit(): void {
   }
 
